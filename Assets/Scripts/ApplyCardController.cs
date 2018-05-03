39733f7fb60ba8f7e5a3d5b07833fc4af3de32a7
Júlia Vila <juliafvila@gmail.com>
2018-05-03 15:52:55 -0300

    melhorando card fixo

---
+++ Assets/Scripts/ApplyCardController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyCardController : MonoBehaviour {
+
+	public GameObject ground;
+	public GameObject fixedModPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void applyFixedMod() {
+		GameObject emptyGameObj = new GameObject();
+		Transform transformAux = transform;
+		transformAux.position += new Vector3(0, 2, 0);
+		GameObject newFixedMod = Instantiate(fixedModPrefab, transformAux.position, transformAux.rotation);
+		emptyGameObj.transform.SetParent(ground.transform);
+		newFixedMod.transform.SetParent(emptyGameObj.transform);
+	}
+}
---
+++ Assets/Scripts/ModController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ModController : MonoBehaviour, ITrackableEventHandler 
+{
+
+	private TrackableBehaviour mTrackableBehaviour;
+	public GameObject buttonApplyCard;
+
+	//fixed mod
+	public string trackableTag = "FixedMod";
+
+	// Use this for initialization
+	void Start () 
+	{
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+
+		buttonApplyCard.gameObject.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			Debug.Log("JULIA: Trackable " + mTrackableBehaviour.TrackableName + "/" + " found");
+			OnTrackingFound();
+		}
+		else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+			newStatus == TrackableBehaviour.Status.NOT_FOUND)
+		{
+			Debug.Log("JULIA: Trackable " + mTrackableBehaviour.TrackableName + " lost");
+			OnTrackingLost();
+		}
+		else OnTrackingLost();
+	}
+
+	private void OnTrackingFound()
+	{
+		Debug.Log (">>>>TAG<<<< " + mTrackableBehaviour.tag);
+
+		if (mTrackableBehaviour.tag == trackableTag) {
+			buttonApplyCard.gameObject.SetActive (true);
+		}
+
+	}
+
+	private void OnTrackingLost()
+	{
+		Debug.Log (">>>>TAG<<<< " + mTrackableBehaviour.tag);
+		if (mTrackableBehaviour.tag == trackableTag) {
+			buttonApplyCard.gameObject.SetActive (false);
+		}
+	}
+}
