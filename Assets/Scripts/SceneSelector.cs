c6ac625d7104173238dfc11bfee33d967496b622
Júlia Vila <juliafvila@gmail.com>
2018-05-06 18:59:00 -0300

    implementando transição de cenas

---
+++ Assets/Scripts/SceneSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+// 		Debug.Log ("JULIA debug do SceneSelector ");
+		
+	}
+
+	public void goToScene(string nameScene) {
+		SceneManager.LoadScene(nameScene, LoadSceneMode.Single);
+	}
+}
