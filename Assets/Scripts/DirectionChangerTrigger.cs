a878b9d8ecc8753bfb3c5f8dfecafdfe90355d25
joaopedrobohn <32331645+joaopedrobohn@users.noreply.github.com>
2018-05-06 22:21:13 -0300

    mudanca de direcao com trigger
    adicionado script para possibilitar mudansa de trageto dos toys atravez de triggers

---
+++ Assets/Scripts/DirectionChangerTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionChangerTrigger : MonoBehaviour {
+
+	public Vector3 dirMod;
+
+	// Use this for initialization
+	void Start () 
+	{
+		dirMod =  transform.forward;
+		transform.gameObject.tag = "DirectionSign";
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Toy") 
+		{
+			col.gameObject.GetComponent<ToyMove>().setMoveDirection(dirMod);
+		}
+	}
+}
