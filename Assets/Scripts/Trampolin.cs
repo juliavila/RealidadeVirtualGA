bc882127732897582c058bf379b230321a75d97a
Júlia Vila <juliafvila@gmail.com>
2018-05-06 23:42:41 -0300

    adicionando modificador de direção

---
+++ Assets/Scripts/Trampolin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampolin : MonoBehaviour {
+
+	public float jumpStr = 1000.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Toy") 
+		{
+			col.gameObject.GetComponent<ToyMove>().moveDir.y = jumpStr;
+		}
+	}
+}
