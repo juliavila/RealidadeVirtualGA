c2f0d02e58136f7450002e82b281988890efc79c
Júlia Vila <juliafvila@gmail.com>
2018-05-09 00:03:52 -0300

    adicionando borracha

---
+++ Assets/Scripts/EraserController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EraserController : MonoBehaviour {
+
+	public GameObject buttonErase;
+	private GameObject mod;
+
+	// Use this for initialization
+	void Start () {
+		buttonErase.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Mod") {
+			mod = col.gameObject;
+			col.gameObject.GetComponent<Renderer> ().material.color = Color.yellow;
+			buttonErase.SetActive (true);
+		}
+	}
+
+	public void erase() {
+		Destroy (mod);
+	}
+		
+}
